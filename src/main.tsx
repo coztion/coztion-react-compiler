e941a6f846f0c28821124ad103b9196838c953e4
coz <codesver@gmail.com>
2025-05-21 23:40:21 +0900

    chore : configure react-scan and cross-env

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { scan } from "react-scan"; // must be imported before React and React DOM
 
 import App from "@/App";
+
+scan({
+  enabled: import.meta.env.VITE_REACT_SCAN === "ENABLED",
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
