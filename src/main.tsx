50b53d4b859c539c5e567e633a3ed198faab498e
coz <codesver@gmail.com>
2025-05-21 23:53:11 +0900

    chore : add compiler disabled script

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { scan } from "react-scan"; // must be imported before React and React DO
 import App from "@/App";
 
 scan({
-  enabled: import.meta.env.VITE_REACT_SCAN === "ENABLED",
+  enabled: import.meta.env.VITE_REACT_SCAN === "SCAN",
 });
 
 createRoot(document.getElementById("root")!).render(<App />);
